cbb09b4109128ae83a35f48d2d711942a96e8c74
Yazan Tarifi <yazantarifi98@gmail.com>
2021-10-07 10:06:45 +0300

    Start Building Firebase Messaging Service Code Generation

--- src/main/kotlin/com/yazantarifi/actions/generate/FirebaseNotificationsServiceGenerator.kt
+++ src/main/kotlin/com/yazantarifi/actions/generate/FirebaseNotificationsServiceGenerator.kt
@@ -3,9 +3,13 @@ package com.yazantarifi.actions.generate
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.yazantarifi.ui.FeatureNameDialog
 import com.yazantarifi.utils.ApplicationUtils
+import com.yazantarifi.utils.FilesUtil
 import com.yazantarifi.utils.GradleManager
+import java.io.File
+import java.io.FileWriter
 
 class FirebaseNotificationsServiceGenerator: AnAction() {
 
@@ -28,6 +32,27 @@ class FirebaseNotificationsServiceGenerator: AnAction() {
 
     private fun executeNewFeatureEvent(project: Project, name: String, event: AnActionEvent) {
         executeGradleDependency(project, event)
+        FilesUtil.getVirtualFileByAction(event)?.let {
+            if (it.exists()) {
+                generateFirebaseServiceFile(it, name, project, event)
+                it.refresh(false, true)
+            }
+        }
+    }
+
+    private fun generateFirebaseServiceFile(targetFile: VirtualFile, name: String, project: Project, event: AnActionEvent) {
+        File(targetFile.path, "${name}FirebaseNotificationsService.kt").apply {
+            val packageName = ApplicationUtils.getPackageNameFromFile(this, project)
+            FileWriter(this, false).apply {
+                try {
+                    this.write("package $packageName\n")
+                    this.flush()
+                    this.close()
+                } catch (ex: Exception) {
+                    println("Exception : ${ex.message}")
+                }
+            }
+        }
     }
 
     private fun executeGradleDependency(project: Project, event: AnActionEvent) {

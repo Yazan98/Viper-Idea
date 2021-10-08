21b6b751c34bb418c61d15dfb85e95351a880969
Yazan Tarifi <yazantarifi98@gmail.com>
2021-10-08 09:41:02 +0300

    Add Activity Theme Default Style Generator Action

---
+++ src/main/kotlin/com/yazantarifi/actions/generate/ApplicationThemeGenerator.kt
@@ -0,0 +1,13 @@
+package com.yazantarifi.actions.generate
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.yazantarifi.ui.MaterialThemeDialogGenerator
+
+class ApplicationThemeGenerator: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        e.project?.let {
+            MaterialThemeDialogGenerator(it).show()
+        }
+    }
+}
---
+++ src/main/kotlin/com/yazantarifi/ui/MaterialThemeDialogGenerator.kt
@@ -0,0 +1,60 @@
+package com.yazantarifi.ui
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.yazantarifi.utils.IdeaNotificationsManager
+import java.awt.BorderLayout
+import java.awt.Toolkit
+import java.awt.datatransfer.Clipboard
+import java.awt.datatransfer.StringSelection
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+class MaterialThemeDialogGenerator constructor(private val project: Project): DialogWrapper(project, true) {
+
+    companion object {
+        private val DIALOG_CONTENT = """
+            <!-- Generated Code To Default Activity Theme -->
+            <style name="Theme.MainScreen" parent="Theme.MaterialComponents.DayNight.DarkActionBar">
+                <item name="colorPrimary">@color/purple_500</item>
+                <item name="colorPrimaryVariant">@color/purple_700</item>
+                <item name="colorOnPrimary">@color/white</item>
+                <item name="colorSecondary">@color/teal_200</item>
+                <item name="colorSecondaryVariant">@color/teal_700</item>
+                <item name="colorOnSecondary">@color/black</item>
+                <item name="windowNoTitle">true</item>
+                <item name="windowActionBar">false</item>
+                <item name="android:statusBarColor" tools:targetApi="l">?attr/colorPrimaryVariant</item>
+            </style>
+        """.trimIndent()
+    }
+
+    init {
+        title = "Activity Theme Content"
+        init()
+    }
+
+    override fun createCenterPanel(): JComponent? {
+        return JPanel(BorderLayout()).apply {
+            add(getThemeContent(), BorderLayout.CENTER)
+        }
+    }
+
+    private fun getThemeContent(): JLabel {
+        return JLabel(DIALOG_CONTENT)
+    }
+
+    override fun doOKAction() {
+        super.doOKAction()
+        copyStringText()
+        IdeaNotificationsManager(project).showNotification("Activity Theme", "Copied To Clipboard !!")
+    }
+
+    private fun copyStringText() {
+        val selection = StringSelection(DIALOG_CONTENT)
+        val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
+        clipboard.setContents(selection, selection)
+    }
+
+}
